1e56d83ce28b11e57ce810d8bc939e9b3ca15ff6
Nikita Yanchevskii <lishslukh@gmail.com>
2023-01-24 14:58:02 +0300

    vuex init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { mapActions } from 'vuex'
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  components: {},
+  mounted() {
+    this.fetchMovies()
   },
-};
+  methods: {
+    ...mapActions('movies', ['fetchMovies'])
+  }
+}
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import movies from './modules/muvies'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
-});
+  modules: {
+    movies
+  }
+})
